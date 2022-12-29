481a7310972a515f36cc52e8f93c03b7a8120668
oceans76 <119086109+oceans76@users.noreply.github.com>
2022-12-28 19:05:34 -0600

    Create App.js

---
+++ part1/1.6/App.js
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGood = () => {
+    setGood(good + 1)
+  }
+  const handleNeutral = () => {
+    setNeutral(neutral+1)
+  }
+  const handleBad= () => {
+    setBad(bad+1)
+  }
+
+  return (
+    <div>
+      <h1>Course Feedback</h1>
+      <button onClick={handleGood}>Good</button>
+      <button onClick={handleNeutral}>Neutral</button>
+      <button onClick={handleBad}>Bad</button>
+      <h2>Results</h2>
+      <p>Good:{good}</p>
+      <p>Neutral:{neutral}</p>
+      <p>Bad:{bad}</p>
+    </div>
+  )
+}
+
+export default App
